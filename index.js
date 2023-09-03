0fa8277e2f68a969387fb51c6e85b5d0624dea4a
Sapir Ohava <ohavasapir@gmail.com>
2023-09-03 16:11:05 +0300

    adding all todos routes(create ,  delete , update , get all , get one by id )

--- index.js
+++ index.js
@@ -16,21 +16,61 @@ app.post('/todos', async (req, res) => {
     const { description } = req.body;
     // thats how i write postgres query
     const newTodo = await pool.query(
-      'INSERT INTO todo (description) VALUES($1)',
+      'INSERT INTO todo (description) VALUES($1) RETURNING *',
       [description]
     );
-    res.json(newTodo);
+    res.json(newTodo.rows[0]);
   } catch (err) {
-    console.log(err.message);
+    console.error(err.message);
   }
 });
 //get all todos
 
+app.get('/todos', async (req, res) => {
+  try {
+    const allTodos = await pool.query('SELECT * FROM todo');
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
 //get a todo
 
-//update a todo
+app.get('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query('SELECT * FROM todo WHERE todo_id = $1', [
+      id,
+    ]);
+    res.json(todo.rows[0]);
+  } catch (error) {
+    console.error(err.message);
+  }
+});
 
+//update a todo
+app.put('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      'UPDATE todo SET description = $1 WHERE todo_id = $2',
+      [description, id]
+    );
+    res.json('Todo was updated!');
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 //delete a todo
+app.delete('/todos/:id', async (req, res) => {
+  const { id } = req.params;
+  const deletedTodo = await pool.query('DELETE FROM todo WHERE todo_id =  $1', [
+    id,
+  ]);
+  res.json('Todo was deleted!');
+});
 
 app.listen(5000, () => {
   console.log('server has started on port 5000');
