74cf157ef1c25038d96ffc044829b2f70189b92f
Sapir Ohava <ohavasapir@gmail.com>
2023-09-03 09:52:06 +0300

    adding more

--- db.js
+++ db.js
@@ -9,3 +9,5 @@ const pool = new Pool({
   port: 5432,
   database: 'perntodo',
 });
+
+module.exports = pool;
--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+// by using the pool , we can run queries with postgres
+const pool = require('./db');
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//ROUTES//
+
+//create a todo
+app.post('/todos', async (req, res) => {
+  try {
+    const { description } = req.body;
+    // thats how i write postgres query
+    const newTodo = await pool.query(
+      'INSERT INTO todo (description) VALUES($1)',
+      [description]
+    );
+    res.json(newTodo);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+//get all todos
+
+//get a todo
+
+//update a todo
+
+//delete a todo
 
 app.listen(5000, () => {
   console.log('server has started on port 5000');
